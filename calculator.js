d8ab27b2b1b509081702ec5ce242a375eb6f9066
E-Trinh <ethantrinh@csus.edu>
2022-05-26 20:18:13 -0700

    Added functionality for operands, operators, and equals buttons

--- calculator.js
+++ calculator.js
@@ -8,6 +8,49 @@ function operate(operandOne, operandTwo, operator) {
     } else if(operator === "/") {
         return operandOne / operandTwo;
     } else {
-        return "Invalid operator"
+        return "Invalid operator";
     }
 }
+
+let operandOne = 0;
+let operator;
+let operandTwo = 0;
+const display = document.querySelector("p");
+
+const operandButtons = document.querySelectorAll(".operand");
+operandButtons.forEach((operand) => {
+    operand.addEventListener("click", (e) => {
+        if (operator === undefined) {
+            display.textContent = display.textContent + e.target.textContent;
+            operandOne = (operandOne * 10) + parseFloat(e.target.textContent);
+        } else {
+            display.textContent = display.textContent + e.target.textContent;
+            operandTwo = (operandTwo * 10) +  parseFloat(e.target.textContent);
+        }
+    });
+});
+
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach((operatorButton) => {
+        operatorButton.addEventListener("click", (e) => {
+        operator = e.target.textContent;
+        display.textContent = "";
+    });
+});
+
+const clearButton = document.querySelector(".del");
+clearButton.addEventListener("click", (e) => {
+    operandOne = 0;
+    operator = undefined;
+    operandTwo = 0;
+    display.textContent = "";
+});
+
+const equalButton = document.querySelector(".equal");
+equalButton.addEventListener("click", (e) => {
+    result = operate(operandOne, operandTwo, operator);
+    display.textContent = result;
+    operandOne = result;
+    operator = undefined;
+    operandTwo = 0;
+});

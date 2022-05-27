ebb41ab0ac7076f50688f09969ccdb54e8367e65
E-Trinh <ethantrinh@csus.edu>
2022-05-26 21:45:38 -0700

    Added functionality for decimals

--- calculator.js
+++ calculator.js
@@ -15,27 +15,58 @@ function operate(operandOne, operandTwo, operator) {
 let operandOne = 0;
 let operator;
 let operandTwo = 0;
+let clearDisplayFlag = false;
 const display = document.querySelector("p");
 
 const operandButtons = document.querySelectorAll(".operand");
 operandButtons.forEach((operand) => {
     operand.addEventListener("click", (e) => {
-        if (operator === undefined) {
-            display.textContent = display.textContent + e.target.textContent;
-            operandOne = (operandOne * 10) + parseFloat(e.target.textContent);
-        } else {
-            display.textContent = display.textContent + e.target.textContent;
-            operandTwo = (operandTwo * 10) +  parseFloat(e.target.textContent);
+        if (clearDisplayFlag) {
+            display.textContent = "";
+            clearDisplayFlag = false;
         }
+        display.textContent = display.textContent + e.target.textContent;
     });
+});
+
+const zeroButton = document.querySelector(".zero");
+zeroButton.addEventListener("click", (e) => {
+    if (display.textContent != "0") {
+        display.textContent = display.textContent + e.target.textContent;
+    }
+});
+
+const decimalButton = document.querySelector(".decimal");
+console.log(decimalButton)
+decimalButton.addEventListener("click", (e) => {
+    if (clearDisplayFlag) {
+        display.textContent = "";
+        clearDisplayFlag = false;
+    }
+    console.log("yeet");
+    if (!(display.textContent.indexOf(".") > -1)) {
+        display.textContent = display.textContent + e.target.textContent;
+    }
 });
 
 const operatorButtons = document.querySelectorAll(".operator");
 operatorButtons.forEach((operatorButton) => {
         operatorButton.addEventListener("click", (e) => {
         operator = e.target.textContent;
+        operandOne = parseFloat(display.textContent);
         display.textContent = "";
     });
+});
+
+const equalButton = document.querySelector(".equal");
+equalButton.addEventListener("click", (e) => {
+    operandTwo = parseFloat(display.textContent);
+    result = operate(operandOne, operandTwo, operator);
+    display.textContent = result;
+    operandOne = result;
+    operator = undefined;
+    operandTwo = 0;
+    clearDisplayFlag = true;
 });
 
 const clearButton = document.querySelector(".del");
@@ -44,13 +75,5 @@ clearButton.addEventListener("click", (e) => {
     operator = undefined;
     operandTwo = 0;
     display.textContent = "";
-});
-
-const equalButton = document.querySelector(".equal");
-equalButton.addEventListener("click", (e) => {
-    result = operate(operandOne, operandTwo, operator);
-    display.textContent = result;
-    operandOne = result;
-    operator = undefined;
-    operandTwo = 0;
+    clearDisplayFlag = false;
 });
